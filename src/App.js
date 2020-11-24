f16cc34116f18cf1f00cfadaafb51091f6d99f33
vakumar0 <kumarvaibhav1602@gmail.com>
2020-11-24 13:27:49 +0530

    --Added HashRouting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home'
 import Luxury from './components/pages/Luxury';
