b804fa6b396df3252800e785333823b9524c903d
vakumar0 <kumarvaibhav1602@gmail.com>
2020-11-24 13:15:17 +0530

    --Added PageNotFound.js

---
+++ src/components/pages/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function PageNotFound () {
+    return (
+     <div>
+         <Link to="/" ></Link>
+     </div>   
+    )
+}
+
+export default PageNotFound;
